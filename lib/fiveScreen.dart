8e24c1bf224c23d1b4449ae71d98ef8fefa9d21a
chaeyeon <yeonicy@naver.com>
2023-05-21 02:40:15 +0900

    fiveScreen.dart modify

--- lib/fiveScreen.dart
+++ lib/fiveScreen.dart
@@ -1,123 +1,237 @@
 import 'package:flutter/material.dart';
-
+import 'fourScreen.dart';
 
 class fiveScreen extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFFF9F9E9),
-        body: Stack(
-          children: [
-            Positioned(
-              left: 55, // 왼쪽 여백
-              top: 250, // 상단 여백
-              child: Container(
-                width: 300,
-                height: 350,
-                decoration: BoxDecoration(
-                  color: Color(0xFFE5DEBA),
-                  borderRadius: BorderRadius.circular(40),
-                ),
-              ),
-            ),
-            Positioned(
-              left:40,
-              top:175,
-              child: Container(
-                width:70,
-                height:70,
-                decoration: BoxDecoration(
-                  color: Color(0xFFE5DEBA),
-                  borderRadius: BorderRadius.circular(46),
-                ),
-              ),
-            ),
-            Positioned(
-              left:55,
-              top:650,
-              child: Container(
-                width:300,
-                height:180,
-                decoration: BoxDecoration(
-                  color: Color(0xFFE5DEBA),
-                  borderRadius: BorderRadius.circular(46),
-                ),
-              ),
-            ),
-            Positioned(
-              left:125,
-              top:160,
-              child: Container(
-                width:70,
-                height:70,
-                decoration: BoxDecoration(
-                  color: Color(0xFFE5DEBA),
-                  borderRadius: BorderRadius.circular(46),
-                ),
-              ),
-            ),
-            Positioned(
-              left: 210,
-              top:160,
-              child: Container(
-                width:70,
-                height:70,
-                decoration: BoxDecoration(
-                  color: Color(0xFFE5DEBA),
-                  borderRadius: BorderRadius.circular(46),
-                ),
-              ),
-            ),
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
 
-            Positioned(
-              left:300,
-              top:175,
-              child: Container(
-                width:70,
-                height:70,
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF9F9E9),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 30),
+          Container(
+            margin: EdgeInsets.only(left: 80),
+            child: Text(
+              '<         2023.5.13        >',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 100),
+              Container(
+                width: 70,
+                height: 70,
                 decoration: BoxDecoration(
-                  color: Color(0xFFE5DEBA),
+                  color: Color(0xFFE7E3D1),
                   borderRadius: BorderRadius.circular(46),
                 ),
               ),
+              SizedBox(width: 30),
+              Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Color(0xFFE7E3D1),
+                  borderRadius: BorderRadius.circular(46),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 0),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              SizedBox(width: 0),
+              Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Color(0xFFE7E3D1),
+                  borderRadius: BorderRadius.circular(46),
+                ),
+              ),
+              SizedBox(width: 180),
+              Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Color(0xFFE7E3D1),
+                  borderRadius: BorderRadius.circular(46),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(right: 0),
+              width: 350,
+              height: 350,
+              decoration: BoxDecoration(
+                color: Color(0xFFE7E3D1),
+                borderRadius: BorderRadius.circular(46),
+              ),
+              child: Column(
+                children: [
+                  Container(), // 상단 공간 차지를 위한 빈 컨테이너
+                  Text(
+                    'today monsil',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ],
+              ),
             ),
-            Positioned(
-              left: 160, // 왼쪽 여백
-              top: 290, // 상단 여백
-              child: Text(
-                'Today Monsil',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
+          ),
+          SizedBox(height: 40),
+          Container(
+            margin: EdgeInsets.only(left: 60),
+            child: Text(
+              'photo',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 0),
+          Container(
+            margin: EdgeInsets.only(left: 60),
+            child: Text(
+              '--------------------------------',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(right: 0),
+              width: 350,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Color(0xFFE7E3D1),
+                borderRadius: BorderRadius.circular(46),
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+          Container(
+            margin: EdgeInsets.only(left: 60),
+            child: Text(
+              'Today',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 0),
+          Container(
+            margin: EdgeInsets.only(left: 60),
+            child: Text(
+              '--------------------------------',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 0),
+          Container(
+            margin: EdgeInsets.only(left: 60),
+            child: Text(
+              'I do ~ ',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(right: 0),
+              width: 350,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Color(0xFFE7E3D1),
+                borderRadius: BorderRadius.circular(46),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.only(left: 60),
+            child: Text(
+              'I feel ~ ',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(right: 0),
+              width: 350,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Color(0xFFE7E3D1),
+                borderRadius: BorderRadius.circular(46),
+              ),
+            ),
+          ),
+
+
+          SizedBox(height: 90),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(right: 0),
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xFFFCF5B6),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => fourScreen()));
+                },
+                child: Text(
+                  'Save',
+                  style: TextStyle(fontSize: 24),
                 ),
               ),
             ),
-            Positioned(
-              left: 100, // 왼쪽 여백
-              top: 100, // 상단 여백
-              child: Text(
-                ' <       2023.5.13       >',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 40, // 왼쪽 여백
-              top: 615, // 상단 여백
-              child: Text(
-                ' photo',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+          SizedBox(height: 100),
+
+        ],
+
       ),
     );
   }
